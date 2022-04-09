b5905aa395bd84aae9e8d6710e7f980ffc0ad90d
mehedi <mehedi.hasan.shuvon@gmail.com>
2022-04-10 03:01:38 +0600

    added exlpore button on profile page

--- js/sidenav.js
+++ js/sidenav.js
@@ -9,10 +9,12 @@ const demo = () => {
     const content = document.getElementById('content');
     const testingClass = document.getElementById('testing-class');
     const footerNavbar = document.getElementById('footer-navbar');
+    const explore = document.getElementById('nav-explore-mobile');
     console.log(sidebar);
 
     sidebar.classList.toggle('active');
     content.classList.toggle('active');
     testingClass.classList.toggle('active');
     footerNavbar.classList.toggle('active');
+    explore.classList.toggle('active');
 }

d43e9af3182d49549d701c843ea6d87fb731679d
mehedi <mehedi.hasan.shuvon@gmail.com>
2022-04-10 00:17:45 +0600

    polishing user profile

--- js/sidenav.js
+++ js/sidenav.js
@@ -7,8 +7,12 @@
 const demo = () => {
     const sidebar = document.getElementById('sidebar');
     const content = document.getElementById('content');
+    const testingClass = document.getElementById('testing-class');
+    const footerNavbar = document.getElementById('footer-navbar');
     console.log(sidebar);
 
     sidebar.classList.toggle('active');
     content.classList.toggle('active');
+    testingClass.classList.toggle('active');
+    footerNavbar.classList.toggle('active');
 }
